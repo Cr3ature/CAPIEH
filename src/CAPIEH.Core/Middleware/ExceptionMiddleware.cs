9c052bcd5e08e2590144ca1e6d8a00ad08aea5e3
David Vanderheyden <david.vanderheyden@sweetmustard.be>
2019-05-03 09:41:07 +0200

    Resolve InvalidOperationException caused by internal Invoke: Set public

--- src/CAPIEH.Core/Middleware/ExceptionMiddleware.cs
+++ src/CAPIEH.Core/Middleware/ExceptionMiddleware.cs
@@ -4,18 +4,18 @@ using System.Threading.Tasks;
 
 namespace CAPIEH
 {
-    internal class ExceptionMiddleware
+    public class ExceptionMiddleware
     {
         private readonly ExceptionHandler _handler;
         private readonly RequestDelegate _next;
 
-        internal ExceptionMiddleware(RequestDelegate next, ExceptionHandler handler)
+        public ExceptionMiddleware(RequestDelegate next, ExceptionHandler handler)
         {
             _next = next;
             _handler = handler;
         }
 
-        internal async Task InvokeAsync(HttpContext httpContext)
+        public async Task InvokeAsync(HttpContext httpContext)
         {
             try
             {
